25b6bb0f5b5b72ec3ac6c5c0bb836c3f187a8760
Monty Dimkpa <cmdimkpa@gmail.com>
2020-05-20 15:52:33 +0100

    Create modulatePackets.js

---
+++ modulatePackets.js
@@ -0,0 +1,25 @@
+// modulate IP packets to MAC packets every interval
+
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3201
+
+let modulationInterval = 50;
+let axios = require('axios');
+let events = 0;
+
+const modulatePackets = async () => {
+    events++;
+    await axios.get(`https://sub-network-lte.herokuapp.com/SubNetworkLTE/PhysicalUplinkControlChannel/Modulation`).then(response => {
+      console.log(`${response.data} (${events})`)
+    }).catch(err => {
+      console.log(err)
+    })
+}
+
+app.listen(PORT, () => {
+    console.log(`@modulator service: now running on port ${PORT}`);
+    let modulator = setInterval(modulatePackets, modulationInterval);
+});
