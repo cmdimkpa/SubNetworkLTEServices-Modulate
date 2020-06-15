d46ad6b38f9544934d5764efef7301384c90a7a1
Monty Dimkpa <cmdimkpa@gmail.com>
2020-06-15 04:37:57 +0100

    Update modulatePackets.js

--- modulatePackets.js
+++ modulatePackets.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3201
 
-let modulationInterval = 500;
+let modulationInterval = 5000;
 let events = 0;
 
 const modulatePackets = async () => {
